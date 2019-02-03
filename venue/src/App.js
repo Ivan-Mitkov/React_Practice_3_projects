ab72e6b082e1a070b57a42a62f8578ad24a478c4
Ivan Mitkov <ivan.mitkov@gmail.com>
2019-02-03 12:20:28 +0200

    add scroll to

--- venue/src/App.js
+++ venue/src/App.js
@@ -1,28 +1,38 @@
 import React, { Component } from "react";
 import "./resources/styles.css";
+import { Element } from "react-scroll";
 
 import Header from "./components/header_footer/Header";
 import Featured from "./components/featured";
 import VenuInfo from "./components/venueInfo";
 import Highlights from "./components/highlights";
-import Pricing from './components/pricing';
-import Location from './components/location';
-import Footer from './components/header_footer/Footer';
+import Pricing from "./components/pricing";
+import Location from "./components/location";
+import Footer from "./components/header_footer/Footer";
 
 class App extends Component {
   render() {
     return (
-      <div
-        className="App"
-        style={{ height: "1500px" }}
-      >
+      <div className="App" style={{ height: "1500px" }}>
         <Header />
-        <Featured />
-        <VenuInfo />
-        <Highlights />
-        <Pricing/>
-        <Location/>
-        <Footer/>
+
+        <Element name="feature">
+          <Featured />
+        </Element>
+        <Element name="info">
+          <VenuInfo />
+        </Element>
+        <Element name="highlights">
+          <Highlights />
+        </Element>
+        <Element name="pricing">
+          <Pricing />
+        </Element>
+        <Element name="Location">
+          <Location />
+        </Element>
+
+        <Footer />
       </div>
     );
   }
--- venue/src/components/header_footer/SideDrawer.js
+++ venue/src/components/header_footer/SideDrawer.js
@@ -1,10 +1,19 @@
 import React from "react";
+import {  scroller } from 'react-scroll'
 
 import Drawer from "@material-ui/core/Drawer";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 
 const SideDrawer = props => {
+  const scrollToElement=(element)=>{
+    scroller.scrollTo(element,{
+      duration:1000,
+      delay:100,
+      smooth:true
+    })
+    props.onClose(false)
+  }
   return (
     <Drawer
       anchor="right"
@@ -12,19 +21,19 @@ const SideDrawer = props => {
       onClose={() => props.onClose(false)}
     >
       <List component="nav">
-        <ListItem button onClick={() => console.log("feature")}>
+        <ListItem button onClick={() =>scrollToElement("feature")}>
           Event starts in
         </ListItem>
-        <ListItem button onClick={() => console.log("info")}>
+        <ListItem button onClick={() => scrollToElement("info")}>
           Venue info
         </ListItem>
-        <ListItem button onClick={() => console.log("highlights")}>
+        <ListItem button onClick={() => scrollToElement('highlights')}>
           Highlights
         </ListItem>
-        <ListItem button onClick={() => console.log("Pricing")}>
+        <ListItem button onClick={() =>scrollToElement("pricing")}>
           Pricing
         </ListItem>
-        <ListItem button onClick={() => console.log("Location")}>
+        <ListItem button onClick={() => scrollToElement("Location")}>
           Location
         </ListItem>
       </List>
