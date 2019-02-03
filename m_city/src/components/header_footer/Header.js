95f8de27a75d515d71a73dfc3f9207ffd02abcb0
Ivan Mitkov <ivan.mitkov@gmail.com>
2019-02-03 14:25:10 +0200

    add AppBar and ToolBar from material-ui in Header

--- m_city/src/components/header_footer/Header.js
+++ m_city/src/components/header_footer/Header.js
@@ -1,18 +1,36 @@
-import React, { Component } from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import ToolBar from '@material-ui/core/Toolbar';
-import Button from '@material-ui/core/Button';
+import React, { Component } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import ToolBar from "@material-ui/core/Toolbar";
+import Button from "@material-ui/core/Button";
 
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export class Header extends Component {
   render() {
     return (
-      <div>
-        Header
-      </div>
-    )
+      <AppBar
+        position="fixed"
+        style={{
+          backgroundColor: "#98c5e9",
+          boxShadow: "none",
+          padding: "10px 0",
+          borderBottom: "2px solid #00285e"
+        }}
+      >
+        <ToolBar style={{ display: "flex" }}>
+          <div style={{ flexGrow: 1 }}>
+            <div className="header_logo">LOGO</div>
+          </div>
+          <Link to="/the_team">
+            <Button color="inherit">Team</Button>
+          </Link>
+          <Link to="/the_matches">
+            <Button color="inherit">Matches</Button>
+          </Link>
+        </ToolBar>
+      </AppBar>
+    );
   }
 }
 
-export default Header
+export default Header;
