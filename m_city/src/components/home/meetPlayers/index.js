39ae39aefa6faff4d4c55c1193a14f02d8698ce8
unknown <ivan.mitkov@gmail.com>
2019-04-17 07:47:13 +0300

    use react reveal to postpone animation with react move

--- m_city/src/components/home/meetPlayers/index.js
+++ m_city/src/components/home/meetPlayers/index.js
@@ -1,78 +1,93 @@
 import React, { Component } from "react";
+import Reveal from "react-reveal/Reveal";
+
 import BackgroundStripes from "../../../Resources/images/stripes.png";
 import { Tag } from "../../ui/misc";
+import HomeCards from "./cards";
 
 export class MeetPlayers extends Component {
-  state = {};
+  state = {
+    show: false
+  };
   render() {
     return (
-      <div
-        className="home_meetplayers"
-        style={{ background: `#ffffff url(${BackgroundStripes})` }}
+      <Reveal
+        fraction={0.7}
+        onReveal={() => {
+          console.log('reveal')
+          this.setState(()=> this.state.show= true );
+        }}
       >
-        <div className="container">
-          <div className="home_meetplayers_wrapper">
-            <div className="home_card_wrappe">card</div>
-            <div className="home_text_wrapper">
-              <div>
-                <Tag
-                  add={{
-                    background: "#0e1731",
-                    fontSize: "100px",
-                    color: "#ffffff",
-                    display: "inline-block",
-                    marginBottom: "20px"
-                  }}
-                >
-                  Meet
-                </Tag>
+        <div
+          className="home_meetplayers"
+          style={{ background: `#ffffff url(${BackgroundStripes})` }}
+        >
+          <div className="container">
+            <div className="home_meetplayers_wrapper">
+              <div className="home_card_wrapper">
+                <HomeCards show={this.state.show} />
               </div>
-              <div>
-                <Tag
-                  add={{
-                    background: "#0e1731",
-                    fontSize: "100px",
-                    color: "#ffffff",
-                    display: "inline-block",
-                    marginBottom: "20px"
-                  }}
-                >
-                  The
-                </Tag>
-              </div>
-              <div>
-                <Tag
-                  add={{
-                    background: "#0e1731",
-                    fontSize: "100px",
-                    color: "#ffffff",
-                    display: "inline-block",
-                    marginBottom: "20px"
-                  }}
-                >
-                  Players
-                </Tag>
-              </div>
-              <div>
-                <Tag
-                  link={true}
-                  linkTo="/the_team"
-                  add={{
-                    background: "#ffffff",
-                    fontSize: "27px",
-                    color: "#0e1731",
-                    display: "inline-block",
-                    marginBottom: "20px",
-                    border:'1px solid #0e1731'
-                  }}
-                >
-                Meet them here
-                </Tag>
+              <div className="home_text_wrapper">
+                <div>
+                  <Tag
+                    add={{
+                      background: "#0e1731",
+                      fontSize: "100px",
+                      color: "#ffffff",
+                      display: "inline-block",
+                      marginBottom: "20px"
+                    }}
+                  >
+                    Meet
+                  </Tag>
+                </div>
+                <div>
+                  <Tag
+                    add={{
+                      background: "#0e1731",
+                      fontSize: "100px",
+                      color: "#ffffff",
+                      display: "inline-block",
+                      marginBottom: "20px"
+                    }}
+                  >
+                    The
+                  </Tag>
+                </div>
+                <div>
+                  <Tag
+                    add={{
+                      background: "#0e1731",
+                      fontSize: "100px",
+                      color: "#ffffff",
+                      display: "inline-block",
+                      marginBottom: "20px"
+                    }}
+                  >
+                    Players
+                  </Tag>
+                </div>
+                <div>
+                  <Tag
+                    link={true}
+                    linkTo="/the_team"
+                    add={{
+                      background: "#ffffff",
+                      fontSize: "27px",
+                      color: "#0e1731",
+                      display: "inline-block",
+                      marginBottom: "20px",
+                      border: "1px solid #0e1731"
+                    }}
+                  >
+                    Meet them here
+                  </Tag>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </Reveal>
     );
   }
 }
