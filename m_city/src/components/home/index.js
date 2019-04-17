da799fe390fb66cc501bd660647a1e877bc4d1da
unknown <ivan.mitkov@gmail.com>
2019-04-17 08:15:28 +0300

    using Zoom for animation of jersey

--- m_city/src/components/home/index.js
+++ m_city/src/components/home/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React from "react";
 
-import Featured from './featured';
-import MatchesHome from './matches';
-import MeetPlayers from './meetPlayers';
+import Featured from "./featured";
+import MatchesHome from "./matches";
+import MeetPlayers from "./meetPlayers";
+import Promotion from "./promotion/";
 
 const Home = () => {
   return (
-    <div className='bck_blue'>
-        <Featured></Featured>
-        <MatchesHome></MatchesHome>
-        <MeetPlayers></MeetPlayers>
+    <div className="bck_blue">
+      <Featured />
+      <MatchesHome />
+      <MeetPlayers />
+      <Promotion />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
---
+++ m_city/src/components/home/promotion/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Enroll from "./Enroll";
+import PromotionAnimation from "./PromotionAnimation";
+
+const Promotion = () => {
+  return (
+    <div className="promotion_wrapper" style={{ background: "#ffffff" }}>
+      <div className="container">
+        <PromotionAnimation />
+      </div>
+    </div>
+  );
+};
+
+export default Promotion;
