6d4a6c11d1f33df27ee3c13d962a451e9dc5a15e
unknown <ivan.mitkov@gmail.com>
2019-04-18 06:26:29 +0300

    show success message

--- m_city/src/components/home/promotion/Enroll.js
+++ m_city/src/components/home/promotion/Enroll.js
@@ -1,93 +1,108 @@
-import React, { Component } from 'react';
-import Fade from 'react-reveal/Fade';
-import FormField from '../../ui/formFields';
-import {validate} from '../../ui/misc';
+import React, { Component } from "react";
+import Fade from "react-reveal/Fade";
+import FormField from "../../ui/formFields";
+import { validate } from "../../ui/misc";
 
 export class Enroll extends Component {
-    state={
-        formError:false,
-        formSuccess:'',
-        formdata:{
-            email:{
-                element:'input',
-                value:'',
-                config:{
-                    name:'email_input',
-                    type:'email',
-                    placeholder:'Enter your email'
-                },
-                validation:{
-                    required:true,
-                    email:true
-                },
-                valid:false,
-                validationMessage:''
-            }
-        }
+  state = {
+    formError: false,
+    formSuccess: "",
+    formdata: {
+      email: {
+        element: "input",
+        value: "",
+        config: {
+          name: "email_input",
+          type: "email",
+          placeholder: "Enter your email"
+        },
+        validation: {
+          required: true,
+          email: true
+        },
+        valid: false,
+        validationMessage: ""
+      }
     }
-    submitForm=(event)=>{
-        event.preventDefault();
-        let dataToSubmit={};
-        let formIsValid=true;
-        console.log(this.state.formdata)
-        for (let key in this.state.formdata) {
-           dataToSubmit[key]=this.state.formdata[key].value;
-           formIsValid=this.state.formdata[key].valid&&formIsValid
-        }
-        if(formIsValid){
-            console.log(dataToSubmit)
-            this.setState({formError:false})
-        }else{
-            console.log('ERROR')
-            this.setState({formError:true})
-        }
-
-       
+  };
+  resetFormSuccess = () => {
+    const newFormdata = { ...this.state.formdata };
+    for (const key in newFormdata) {
+      newFormdata[key].value = "";
+      newFormdata[key].valid = false;
+      newFormdata[key].validationMessage = "";
     }
+    this.setState({
+      formError: false,
+      formdata: newFormdata,
+      formSuccess: "Success"
+    });
+    this.clearSuccessMessage();
+  };
 
-    updateForm=(element)=>{
-        const newFormdata={...this.state.formdata};
-        const newElement={...newFormdata[element.id]}
+  clearSuccessMessage=()=>{
+    setTimeout(()=>{
+        this.setState({formSuccess:''})
+    },3000)
+  }
 
-        newElement.value=element.event.target.value;
+  submitForm = event => {
+    event.preventDefault();
+    let dataToSubmit = {};
+    let formIsValid = true;
+    // console.log(this.state.formdata)
+    for (let key in this.state.formdata) {
+      dataToSubmit[key] = this.state.formdata[key].value;
+      formIsValid = this.state.formdata[key].valid && formIsValid;
+    }
+    if (formIsValid) {
+      this.resetFormSuccess();
+    } else {
+      console.log("ERROR");
+      this.setState({ formError: true });
+    }
+  };
 
-        let validData=validate(newElement)
-        // console.log(validData)
-        newElement.valid=validData[0];
-        newElement.validationMessage=validData[1];
+  updateForm = element => {
+    const newFormdata = { ...this.state.formdata };
+    const newElement = { ...newFormdata[element.id] };
 
+    newElement.value = element.event.target.value;
 
-        newFormdata[element.id]=newElement;
-        this.setState({formdata:newFormdata,formError:false});
+    let validData = validate(newElement);
+    // console.log(validData)
+    newElement.valid = validData[0];
+    newElement.validationMessage = validData[1];
+
+    newFormdata[element.id] = newElement;
+    this.setState({ formdata: newFormdata, formError: false });
     //    console.log(this.state.formdata);
+  };
 
-    }
-   
   render() {
     return (
       <Fade>
         <div className="enroll_wrapper">
-            <form onSubmit={(e)=>this.submitForm(e)}>
-                <div className="enroll_title">
-                    Enter your email
-                </div>
-                <div className="enroll_input">
-                    <FormField
-                        id={'email'}
-                        formdata={this.state.formdata.email}                   
-                        change={(element)=>this.updateForm(element)}
-                    />
-                    {/* <button onClick={(event)=>this.submitForm(event)}>Enroll</button> */}
-                    {this.state.formError&&<div className='error_label'>Error in the form</div>}
-                    <button type='submit'>Enroll</button>
-                 
-                </div>
-            </form>
+          <form onSubmit={e => this.submitForm(e)}>
+            <div className="enroll_title">Enter your email</div>
+            <div className="enroll_input">
+              <FormField
+                id={"email"}
+                formdata={this.state.formdata.email}
+                change={element => this.updateForm(element)}
+              />
+              {/* <button onClick={(event)=>this.submitForm(event)}>Enroll</button> */}
+              {this.state.formError && (
+                <div className="error_label">Error in the form</div>
+              )}
+              <div className="success_label">{this.state.formSuccess}</div>
+              <button type="submit">Enroll</button>
+            </div>
+          </form>
         </div>
       </Fade>
-    )
+    );
   }
 }
 
-export default Enroll
-
+export default Enroll;
