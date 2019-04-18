327d34605ae9476d73aee8a93d78da0d7d629f67
unknown <ivan.mitkov@gmail.com>
2019-04-18 06:50:17 +0300

    push email to firebase DB finish home page

--- m_city/src/components/home/promotion/Enroll.js
+++ m_city/src/components/home/promotion/Enroll.js
@@ -3,6 +3,8 @@ import Fade from "react-reveal/Fade";
 import FormField from "../../ui/formFields";
 import { validate } from "../../ui/misc";
 
+import { firebasePromotions } from "../../../firebase";
+
 export class Enroll extends Component {
   state = {
     formError: false,
@@ -25,7 +27,7 @@ export class Enroll extends Component {
       }
     }
   };
-  resetFormSuccess = () => {
+  resetFormSuccess = type => {
     const newFormdata = { ...this.state.formdata };
     for (const key in newFormdata) {
       newFormdata[key].value = "";
@@ -35,16 +37,16 @@ export class Enroll extends Component {
     this.setState({
       formError: false,
       formdata: newFormdata,
-      formSuccess: "Success"
+      formSuccess: type ? "Success" : "Already on the database"
     });
     this.clearSuccessMessage();
   };
 
-  clearSuccessMessage=()=>{
-    setTimeout(()=>{
-        this.setState({formSuccess:''})
-    },3000)
-  }
+  clearSuccessMessage = () => {
+    setTimeout(() => {
+      this.setState({ formSuccess: "" });
+    }, 3000);
+  };
 
   submitForm = event => {
     event.preventDefault();
@@ -56,7 +58,19 @@ export class Enroll extends Component {
       formIsValid = this.state.formdata[key].valid && formIsValid;
     }
     if (formIsValid) {
-      this.resetFormSuccess();
+      //check if the user is in the database
+      firebasePromotions
+        .orderByChild("email")
+        .equalTo(dataToSubmit.email)
+        .once("value")
+        .then(snapshot => {
+          if (snapshot.val() === null) {
+            firebasePromotions.push(dataToSubmit);
+            this.resetFormSuccess(true);
+          } else {
+            this.resetFormSuccess(false);
+          }
+        });
     } else {
       console.log("ERROR");
       this.setState({ formError: true });
@@ -97,6 +111,10 @@ export class Enroll extends Component {
               )}
               <div className="success_label">{this.state.formSuccess}</div>
               <button type="submit">Enroll</button>
+              <div className="enroll_discl">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Tempora est rerum quae eum, exercitationem ut magnam architect
+              </div>
             </div>
           </form>
         </div>
