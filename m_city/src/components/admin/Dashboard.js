5b2301323d15bdb580ad043f97d70d23f9e9f90f
unknown <ivan.mitkov@gmail.com>
2019-04-18 08:42:24 +0300

    Dashboard and AdminNav

---
+++ m_city/src/components/admin/Dashboard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import AdminLayout from "../../hoc/AdminLayout";
+
+const Dashboard = () => {
+  return (
+    <AdminLayout>
+      <div className="user_dashboard">
+        <div>Dashboard</div>
+      </div>
+    </AdminLayout>
+  );
+};
+
+export default Dashboard;
