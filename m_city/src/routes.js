5443c5944c80b10f963bc880a8a7538b5465a130
unknown <ivan.mitkov@gmail.com>
2019-04-18 07:39:25 +0300

    form for auth basic - from enroll form

--- m_city/src/routes.js
+++ m_city/src/routes.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
-import Layout from './hoc/Layout';
-import Home from './components/home';
+import Layout from "./hoc/Layout";
+import Home from "./components/home";
+import SignIn from "./components/signin/";
 
-const Routes = (props)=> {
- 
-    return (
-     <Layout>
-        <Switch>
-            <Route exact component={Home} path='/'></Route>
-        </Switch>
-     </Layout>
-    );
- 
-}
+const Routes = props => {
+  return (
+    <Layout>
+      <Switch>
+        <Route exact component={SignIn} path="/sign_in" />
+        <Route exact component={Home} path="/" />
+      </Switch>
+    </Layout>
+  );
+};
 
 export default Routes;
